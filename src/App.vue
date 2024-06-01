9407cd82c1e1229256279ae1f88c2b5adde76d16
John <johnfe@outlook.es>
2024-05-31 19:46:49 -0500

    taller_15

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Mi primer proyeco Vue - John Felipe Ramos"/>
 </template>
 
 <script>
